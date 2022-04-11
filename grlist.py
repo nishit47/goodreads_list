56855c7d9578c572df18db2aa958860bb0aa81ed
nishit47 <nishitthapa47@gmail.com>
2022-04-11 14:45:59 +0545

    changed so that all books can be scraped
    any goodreads list only has upto 100 pages so only 10000 books can be scraped out of any list but there is nothing one can do about it

--- grlist.py
+++ grlist.py
@@ -27,18 +27,18 @@ for character in numberOfBooksString:
 
 numberOfPages=(int(numberOfBooks)//100)+1
 loopNumber=numberOfPages+2
+print(loopNumber)
 
 with open(dataNameOutput, 'w', encoding='utf8', newline='') as f:
     thewriter=writer(f)
     heading=["Rank", "Name", "Author", "Rating"]
     thewriter.writerow(heading)
 
-    for pageNumber in range(2,loopNumber):
-
+    for pageNumber in range(2,loopNumber): #pageNumber=1+actualPageNumber because url changes after the inner loop
         page=requests.get(url)
         soup= BeautifulSoup(page.content, 'html.parser')
         lists=soup.find_all('tr')
-
+        
         for list in lists:
             ranking=list.find('td', class_="number").text
             title=list.find('a', class_="bookTitle").span.text
@@ -55,9 +55,12 @@ with open(dataNameOutput, 'w', encoding='utf8', newline='') as f:
         #going to the next page number
         if pageNumber==2:
             url=url+"?page="+str(pageNumber)
-        else:
+        elif pageNumber<11:
             url=url[:-1]+str(pageNumber)
-
+        elif pageNumber>10 and pageNumber<101:
+            url=url[:-2]+str(pageNumber)
+        else:
+            break # because goodreads only has 10000 viewable books on a list
 
 df=pd.read_csv(dataNameOutput)
 
